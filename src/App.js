a665fd4b85374b78e7c0154fe14147753d55c55b
johnmccants002 <johnmccants002@gmail.com>
2022-08-21 17:52:28 -0700

    Create and Get request

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios"
+import React from "react"
 
+const baseURL = "https://jsonplaceholder.typicode.com/posts";
 function App() {
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(`${baseURL}/1`).then((response) => {
+      setPost(response.data)
+    });
+  }, []);
+
+  function createPost() {
+    axios
+      .post(baseURL, {
+        title: "Hello World",
+        body: "This is a new post"
+      })
+      .then((response) => {
+        console.log("Response: ", response.data)
+        setPost(response.data)
+      })
+  }
+
+  if (!post) return null;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+      <button onClick={createPost}>Create Post</button>
     </div>
   );
 }
